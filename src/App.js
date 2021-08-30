8c452344b7efd31a3176f4a884b77f92367fb08e
Jayan <sunil.chdry@gmail.com>
2021-08-30 17:07:34 +0530

    added code to change reminder value in the json server

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ function App() {
 
     return data;
   };
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    console.log(data);
+
+    return data;
+  };
 
   //Add Task
 
@@ -60,12 +68,24 @@ function App() {
 
   // toggle reminder
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     console.log(id);
 
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
